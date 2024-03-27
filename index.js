c314e8da3df9e8e75ad578e2593cdfc5ae2b0d63
mekhitudor <mekhictudor@gmail.com>
2024-03-27 17:12:57 -0400

    FIXED question 4 done

--- index.js
+++ index.js
@@ -23,11 +23,10 @@ const multiplesOfThreeOrFive = () => {
 }
 multiplesOfThreeOrFive()
 
-const multiplesOfThreeOrFive = () => {
-    for (let i = 1; i < 101; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            console.log(i)
-        }
+const untilNum = (num) => {
+    for (let i = 1; i <= num; i++) {
+        console.log(i)
     }
+
 }
-multiplesOfThreeOrFive()
+untilNum(5)
